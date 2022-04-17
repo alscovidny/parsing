3e6f9a5c1a3312fbb0e2ba4f42a91feda9423e56
Alexander <sagaradze96@gmail.com>
2022-04-17 13:10:08 +0300

    сделали парсинг одной страницы

--- hw 2 sagaradze.py
+++ hw 2 sagaradze.py
@@ -6,43 +6,57 @@ headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/
 
 url = 'https://hh.ru/vacancies/khimik?page=0&hhtmFrom=vacancy_search_catalog'
 
-# url = 'https://python-scripts.com/requests'
+url_parse = bs(url, 'html.parser')
+url_parse.find('page')
 
 # r = requests.get(url, headers=headers)
+
+
 
 # with open('hh_himiki.html', 'wt', encoding='utf-8') as f:
     # f.write(r.text)
 
-html_file = ''
-with open('hh_himiki.html', 'r', encoding='utf-8') as f:
-    html_file = f.read()
+# html_file = ''
+# with open('hh_himiki.html', 'r', encoding='utf-8') as f:
+#     html_file = f.read()
 
-soup = bs(html_file, 'html.parser')
+soup = bs(r.text, 'html.parser')
 
-
+# soup = bs(html_file, 'html.parser')
 
 info_full_list = soup.find_all('div', attrs={'class' : 'vacancy-serp-item-body__main-info'})
 
+is_next_page = soup.find('a', attrs={'data-qa' : 'pager-next'}).get('href')
+
+# print(is_next_page)
+
+data = []
+
 for info_full in info_full_list:
+
+    vacancy_data = {}
 
     vacancy = info_full.find('a', attrs={'data-qa' : 'vacancy-serp__vacancy-title'})
     vacancy_cash = info_full.find('span', attrs={'data-qa' : 'vacancy-serp__vacancy-compensation'})
 
-    a = []
-
-    a.append(vacancy.text) #даёт название профессииa
-    a.append(vacancy.get('href'))
-
-    pprint(a)
+    vacancy_data['1.вакансия'] = vacancy.text
+    vacancy_data['5.ссылка'] = vacancy.get('href')
+    vacancy_data['2.минимальная ЗП'] = None
+    vacancy_data['3.максимальная ЗП'] = None
+    vacancy_data['4.валюта'] = None
 
     if vacancy_cash:
-        print(vacancy_cash.text)
-    else:
-        print('no cash')
+        cash_list = vacancy_cash.text.replace('\u202f','').split()
+        vacancy_data['4.валюта'] = cash_list.pop()
+        # print(cash_list)
+        if 'от' in cash_list:
+            vacancy_data['2.минимальная ЗП'] = int(cash_list.pop())
+        elif 'до' in cash_list:
+            vacancy_data['3.максимальная ЗП'] = int(cash_list.pop())
+        else:
+            vacancy_data['2.минимальная ЗП'] = int(cash_list[0])
+            vacancy_data['3.максимальная ЗП'] = int(cash_list.pop())
 
+    data.append(vacancy_data)
 
-
-
-# pprint(info_full)
-# pprint(info.text)
-# print(soup.head)
+pprint(data)
