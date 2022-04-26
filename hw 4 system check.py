c2707f5f1f32e26def0acd748087e7c72e61124d
Alexander <sagaradze96@gmail.com>
2022-04-26 22:46:17 +0300

    парсит главную табличку

---
+++ hw 4 system check.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient('mongodb://localhost:27017')
+
+db = client['news']['mailru']
+for doc in db.find({}):
+    pprint(doc)
+
+print(f'\nВсего документов в базе: {db.count_documents({})}')
--- hw 4.py
+++ hw 4.py
@@ -1,32 +1,35 @@
 from lxml import html
 import requests
-from pprint import pprint
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+import hashlib
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['news']['mailru']
 
 url = 'https://news.mail.ru/'
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.141 YaBrowser/22.3.2.644 Yowser/2.5 Safari/537.36'}
 
-# response = requests.get(url, headers=header)
-
-# dom = html.fromstring(response.text)
-
-# href_list = dom.xpath("//a[contains(@class, 'js-topnews__item')]/@href")
-
-# pprint(href_list)
-
-# for ref in href_list:
-ref = 'https://news.mail.ru/politics/51056244/'
-
-response = requests.get(ref, headers=header)
+response = requests.get(url, headers=header)
 dom = html.fromstring(response.text)
 
-cur_path = dom.xpath("//div[contains(@class, 'breadcrumbs_article')]")
+main_table = dom.xpath("//[contains(@class, 'js-topnews__item')]/@href")
+# main_table = dom.xpath("//div[@class, 'js-module']")
 
-# print(cur_path)
-for cur in cur_path:
+for ref in href_list:
 
-    time = cur.xpath("./*//span[contains(@class, 'js-ago')]/@datetime")
-    source = cur.xpath("./*//span[contains(@class, 'link__text')]/text()")
-    name = cur.xpath("./../*//h1[contains(@class, 'hdr__inner')]/text()")
+    response = requests.get(ref, headers=header)
+    dom = html.fromstring(response.text)
+    cur_path = dom.xpath("//div[contains(@class, 'breadcrumbs_article')]")
 
+    news = {}
+    news['_id'] = hashlib.sha1(ref.encode('utf_8')).hexdigest()
+    news['дата публикации'] = str(*cur_path[0].xpath("./*//span[contains(@class, 'js-ago')]/@datetime")).split('T')[0]
+    news['источник'] = cur_path[0].xpath("./*//span[contains(@class, 'link__text')]/text()")[0]
+    news['заголовок'] = str(*cur_path[0].xpath("./../*//h1[contains(@class, 'hdr__inner')]/text()"))
+    news['ссылка'] = ref
 
-    print(str(*time).split('T')[0])
+    try:
+        db.insert_one(news)
+    except DuplicateKeyError:
+        pass
