1511edb26fd29e0dda6f5eca5e07125c86725c29
Alexander <sagaradze96@gmail.com>
2022-04-27 00:12:51 +0300

    сделал парсинг новостей (кроме спорта)

--- hw 4.py
+++ hw 4.py
@@ -13,47 +13,35 @@ header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/53
 response = requests.get(url, headers=header)
 dom = html.fromstring(response.text)
 
-# main_table = dom.xpath("//a[contains(@class, 'js-topnews__item')]/@href")
 main_tag = dom.xpath("//div[contains(@data-counter-type, 'topmail')]")[0]
 
-l = main_tag
-
-main_table = l.xpath("./*//div[@class ='js-module']/*//a[contains(@class, 'js-topnews__item')]/@href")
-main_undertable = l.xpath("./*//div[@class ='js-module']/*//a[contains(@class, 'list__text')]/@href")
-
-# main_topics = l.xpath("./../*//div[contains(@data-logger, 'news__MainRubricNews')]/*"
-#                       "//div[@class='cols__wrapper']/*"
-#                       "//a[contains(@class, 'link')]/@href")
-
-region_topics_news = l.xpath("./../*//div[contains(@data-logger, 'news__MainRegionNews')"
+main_table = main_tag.xpath("./*//div[@class ='js-module']/*//a[contains(@class, 'js-topnews__item')]/@href")
+main_undertable = main_tag.xpath("./*//div[@class ='js-module']/*//a[contains(@class, 'list__text')]/@href")
+region_topics_news = main_tag.xpath("./../*//div[contains(@data-logger, 'news__MainRegionNews')"
                         "or contains(@data-logger, 'news__MainRubricNews')]/*"
                       "//div[@class='cols__wrapper']/*"
                       "//a[contains(@class, 'link')]/@href")
 
-all_news = main_table + main_undertable + region_topics_news
+href_list = main_table + main_undertable + region_topics_news
 
-# /*//a[@contains(@class, 'link')]/@href
-# //div[@data-logger='news__MainRubricNews']/*
-# div[@class='cols__wrapper']/*//
-#
-# print(main_table)
-# print(main_undertable)
-print(region_topics_news)
-#
-# for ref in href_list:
-#
-#     response = requests.get(ref, headers=header)
-#     dom = html.fromstring(response.text)
-#     cur_path = dom.xpath("//div[contains(@class, 'breadcrumbs_article')]")
-#
-#     news = {}
-#     news['_id'] = hashlib.sha1(ref.encode('utf_8')).hexdigest()
-#     news['дата публикации'] = str(*cur_path[0].xpath("./*//span[contains(@class, 'js-ago')]/@datetime")).split('T')[0]
-#     news['источник'] = cur_path[0].xpath("./*//span[contains(@class, 'link__text')]/text()")[0]
-#     news['заголовок'] = str(*cur_path[0].xpath("./../*//h1[contains(@class, 'hdr__inner')]/text()"))
-#     news['ссылка'] = ref
-#
-#     try:
-#         db.insert_one(news)
-#     except DuplicateKeyError:
-#         pass
+for ref in href_list:
+
+    if 'sportmail.ru' in ref: # пропустил новости спорта, поскольку для них не указывается источник и используется
+        pass                  # совершенно другая верстка
+
+    response = requests.get(ref, headers=header)
+    dom = html.fromstring(response.text)
+    cur_path = dom.xpath("//div[contains(@class, 'breadcrumbs_article')]")
+
+    news = {}
+    news['_id'] = hashlib.sha1(ref.encode('utf_8')).hexdigest()
+
+    news['дата публикации'] = str(*cur_path[0].xpath("./*//span[contains(@class, 'js-ago')]/@datetime")).split('T')[0]
+    news['источник'] = cur_path[0].xpath("./*//span[contains(@class, 'link__text')]/text()")[0]
+    news['заголовок'] = str(*cur_path[0].xpath("./../*//h1[contains(@class, 'hdr__inner')]/text()"))
+    news['ссылка'] = ref
+
+    try:
+        db.insert_one(news)
+    except DuplicateKeyError:
+        pass
